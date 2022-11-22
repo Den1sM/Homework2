fd237a33fa9b7839b89b8b50a061a8d498375a76
Denis <malaevdv@gmail.com>
2022-11-22 12:40:51 +0300

    First commit

---
+++ Task_10/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
+Console.Write("Введите трёхзначное число: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+string NumElements = Convert.ToString(Num);
+Console.WriteLine("Второй цифрой числа является "+ NumElements [1]);
---
+++ Task_13/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите трёхзначное число: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+string NumElements = Convert.ToString(Num);
+if (NumElements.Length > 2) 
+{
+Console.WriteLine("Третьей цифрой числа является "+ NumElements [2]);
+}
+else
+{
+Console.WriteLine("Третьей цифры нет");  
+}
+Console.ReadKey();
